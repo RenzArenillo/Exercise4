e642c02b5602c0762a0d910631edb7f64de00b74
RENZ ARENILLO <rarenillo@77global.biz>
2022-09-29 23:13:53 +0800

    no edit

--- src/app/blog/pages/blog-list/blog-list.component.ts
+++ src/app/blog/pages/blog-list/blog-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Blog } from '../../models/blog';
 import { BlogService } from '../../services/blog.service';
 
@@ -14,7 +15,7 @@ export class BlogListComponent implements OnInit {
 
   public blogs : Blog[] = [];
 
-  constructor(private list:BlogService) {
+  constructor(private list:BlogService, private router: Router) {
    }
 
   ngOnInit(): void {
@@ -28,12 +29,13 @@ export class BlogListComponent implements OnInit {
   }
 
   editBook(i:number) {
-    
+    this.router.navigate(['/blog','form'])
   }
 
   execAction(event: {data: {name: string, desc: string}, action: string}) {
     switch(event.action) {
       case 'add':
+        this.router.navigate(['/blog','form'])
         break
       case 'deleteAll':
         break
